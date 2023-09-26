9cd1a8ee8ef31983318bf0771612df5c95c32612
久三年 <yuan429021523@qq.com>
2023-09-26 17:30:48 +0800

    jellyfin 修改

--- Jellyfin/jellyfin.sh
+++ Jellyfin/jellyfin.sh
@@ -2,16 +2,16 @@
 
 APP_HOME=/zscloud/cache/app
 mkdir -p $APP_HOME/jellyfin/config
-
+mkdir -p $APP_HOME/Media/tv
+mkdir -p $APP_HOME/Media/movies
 
 docker run --name jellyfin\
     -e PUID=1000 \
     -e PGID=1000 \
     -e TZ=Asia/Shanghai \
-    -e AUTO_UPDATE=true \
     -p 8097:8096 \
     -v $APP_HOME/jellyfin/config:/config \
-    -v $APP_HOME/Media:/Media  \
-    -v /opt/vc/lib:/opt/vc/lib  \
+    -v $APP_HOME/Media/tv:/data/tvshows \
+    -v $APP_HOME/Media/movies:/data/movies \
     --restart unless-stopped \
-    -d linuxserver/jellyfin:10.8.10
+    -d linuxserver/jellyfin:latest
